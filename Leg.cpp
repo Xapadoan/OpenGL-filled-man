9dc2bb89d3434501437cb535ecfd7b7edd4e832c
Xapadoan <HollyolGamu@hotmail.com>
2022-11-03 14:54:29 +0100

    add knees

--- Leg.cpp
+++ Leg.cpp
@@ -3,19 +3,16 @@
 
 Leg::Leg(RelativePoint &anchor, bool side) :
 	_articulation(anchor, {90.0f, 45.0f, 45.0f}, {-90.0f, -45.0f, -45.0f}),
+	_knee(Point(0.0f, -0.5f, 0.0f), {80.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}),
 	_side(side)
 {
 }
 
-void	Leg::render(void)
+void Leg::renderMollet(void)
 {
-	glTranslatef(this->_articulation.x(), this->_articulation.y(), this->_articulation.z());
-	glRotatef(this->_articulation.rx(), 1.0f, 0.0f, 0.0f);
-	glRotatef(this->_articulation.ry(), 0.0f, 1.0f, 0.0f);
-	glRotatef(this->_articulation.rz(), 0.0f, 0.0f, 1.0f);
-
 	glBegin(GL_TRIANGLES);
 	if (this->_side == 1) {
+		// top
 		glVertex3f(0.0f, 0.0f, 0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
 		glVertex3f(0.2f, 0.0f, -0.1f);
@@ -24,46 +21,51 @@ void	Leg::render(void)
 		glVertex3f(0.0f, 0.0f, 0.1f);
 		glVertex3f(0.2f, 0.0f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
-		glVertex3f(0.2f, -1.0f, -0.1f);
+		// bottom
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
 
-		glVertex3f(0.2f, -1.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
-		glVertex3f(0.2f, -1.0f, 0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
 
+		// inside
 		glVertex3f(0.0f, 0.0f, 0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 
+		// outside
 		glVertex3f(0.2f, 0.0f, 0.1f);
 		glVertex3f(0.2f, 0.0f, -0.1f);
-		glVertex3f(0.2f, -1.0f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
 
-		glVertex3f(0.2f, -1.0f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
 		glVertex3f(0.2f, 0.0f, -0.1f);
-		glVertex3f(0.2f, -1.0f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
 
+		// back
 		glVertex3f(0.2f, 0.0f, -0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 		glVertex3f(0.2f, 0.0f, -0.1f);
-		glVertex3f(0.2f, -1.0f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
 
+		// front
 		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex3f(0.2f, 0.0f, 0.1f);
 		glVertex3f(0.0f, 0.0f, 0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 		glVertex3f(0.2f, 0.0f, 0.1f);
-		glVertex3f(0.2f, -1.0f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
 
 		glColor3f(0.0f, 1.0f, 0.0f);
 	} else {
@@ -75,50 +77,188 @@ void	Leg::render(void)
 		glVertex3f(0.0f, 0.0f, 0.1f);
 		glVertex3f(-0.2f, 0.0f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
-		glVertex3f(-0.2f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
 
-		glVertex3f(-0.2f, -1.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
-		glVertex3f(-0.2f, -1.0f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
 
 		glVertex3f(0.0f, 0.0f, 0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 
 		glVertex3f(-0.2f, 0.0f, 0.1f);
 		glVertex3f(-0.2f, 0.0f, -0.1f);
-		glVertex3f(-0.2f, -1.0f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
 
-		glVertex3f(-0.2f, -1.0f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
 		glVertex3f(-0.2f, 0.0f, -0.1f);
-		glVertex3f(-0.2f, -1.0f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
 
 		glVertex3f(-0.2f, 0.0f, -0.1f);
 		glVertex3f(0.0f, 0.0f, -0.1f);
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 
-		glVertex3f(0.0f, -1.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
 		glVertex3f(-0.2f, 0.0f, -0.1f);
-		glVertex3f(-0.2f, -1.0f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
 
 		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex3f(-0.2f, 0.0f, 0.1f);
 		glVertex3f(0.0f, 0.0f, 0.1f);
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 
-		glVertex3f(0.0f, -1.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
 		glVertex3f(-0.2f, 0.0f, 0.1f);
-		glVertex3f(-0.2f, -1.0f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
 
 		glColor3f(0.0f, 1.0f, 0.0f);
 	}
 	glEnd();
+}
+
+void Leg::renderTight(void)
+{
+	glBegin(GL_TRIANGLES);
+	if (this->_side == 1) {
+		// top
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.2f, 0.0f, -0.1f);
+
+		glVertex3f(0.2f, 0.0f, -0.1f);
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.2f, 0.0f, 0.1f);
+
+		// bottom
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
+
+		glVertex3f(0.2f, -0.5f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
+
+		// inside
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+
+		// outside
+		glVertex3f(0.2f, 0.0f, 0.1f);
+		glVertex3f(0.2f, 0.0f, -0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
+
+		glVertex3f(0.2f, -0.5f, 0.1f);
+		glVertex3f(0.2f, 0.0f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
+
+		// back
+		glVertex3f(0.2f, 0.0f, -0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(0.2f, 0.0f, -0.1f);
+		glVertex3f(0.2f, -0.5f, -0.1f);
+
+		// front
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.2f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.2f, 0.0f, 0.1f);
+		glVertex3f(0.2f, -0.5f, 0.1f);
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+	} else {
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(-0.2f, 0.0f, 0.1f);
+
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
+
+		glVertex3f(-0.2f, -0.5f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
+
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+
+		glVertex3f(-0.2f, 0.0f, 0.1f);
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
+
+		glVertex3f(-0.2f, -0.5f, 0.1f);
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
+
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+		glVertex3f(0.0f, 0.0f, -0.1f);
+		glVertex3f(0.0f, -0.5f, -0.1f);
+
+		glVertex3f(0.0f, -0.5f, -0.1f);
+		glVertex3f(-0.2f, 0.0f, -0.1f);
+		glVertex3f(-0.2f, -0.5f, -0.1f);
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(-0.2f, 0.0f, 0.1f);
+		glVertex3f(0.0f, 0.0f, 0.1f);
+		glVertex3f(0.0f, -0.5f, 0.1f);
+
+		glVertex3f(0.0f, -0.5f, 0.1f);
+		glVertex3f(-0.2f, 0.0f, 0.1f);
+		glVertex3f(-0.2f, -0.5f, 0.1f);
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+	}
+	glEnd();
+}
+
+void	Leg::render(void)
+{
+	glTranslatef(this->_articulation.x(), this->_articulation.y(), this->_articulation.z());
+	glRotatef(this->_articulation.rx(), 1.0f, 0.0f, 0.0f);
+	glRotatef(this->_articulation.ry(), 0.0f, 1.0f, 0.0f);
+	glRotatef(this->_articulation.rz(), 0.0f, 0.0f, 1.0f);
+
+	this->renderTight();
+
+	glTranslatef(this->_knee.dx(), this->_knee.dy(), this->_knee.dz());
+	glRotatef(this->_knee.drx(), 1.0f, 0.0f, 0.0f);
+	glRotatef(this->_knee.dry(), 0.0f, 1.0f, 0.0f);
+	glRotatef(this->_knee.drz(), 0.0f, 0.0f, 1.0f);
+
+	this->renderMollet();
+
+	glRotatef(-this->_knee.drz(), 0.0f, 0.0f, 1.0f);
+	glRotatef(-this->_knee.dry(), 0.0f, 1.0f, 0.0f);
+	glRotatef(-this->_knee.drx(), 1.0f, 0.0f, 0.0f);
+	glTranslatef(-this->_knee.dx(), -this->_knee.dy(), -this->_knee.dz());
+
 	glRotatef(-this->_articulation.rz(), 0.0f, 0.0f, 1.0f);
 	glRotatef(-this->_articulation.ry(), 0.0f, 1.0f, 0.0f);
 	glRotatef(-this->_articulation.rx(), 1.0f, 0.0f, 0.0f);
