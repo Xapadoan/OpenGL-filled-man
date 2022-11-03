825fdb8b7383339de97bc5c8d8075717280c9494
Xapadoan <HollyolGamu@hotmail.com>
2022-11-03 12:26:22 +0100

    Init max r as vector

--- Articulation.hh
+++ Articulation.hh
@@ -2,20 +2,33 @@
 #define ARTICULATION_HH
 
 #include <GL/glut.h>
+#include <vector>
 #include "RelativePoint.hh"
 
 class Articulation
 {
 	public:
-		Articulation(RelativePoint &pos, GLfloat rx, GLfloat ry, GLfloat rz);
+		Articulation(RelativePoint &pos, std::vector<GLfloat> min_r, std::vector<GLfloat> max_r);
 		GLfloat	rx(void);
 		GLfloat	ry(void);
 		GLfloat rz(void);
+		GLfloat	x(void);
+		GLfloat	y(void);
+		GLfloat z(void);
+		Articulation	*setRx(GLfloat rx);
+		Articulation	*setRy(GLfloat ry);
+		Articulation	*setRz(GLfloat rz);
 	private:
 		RelativePoint	&_pos;
 		GLfloat				_rx = 0.0f;
 		GLfloat				_ry = 0.0f;
 		GLfloat				_rz = 0.0f;
+		GLfloat				_max_rx = 180.0f;
+		GLfloat				_min_rx = -180.0f;
+		GLfloat				_max_ry = 180.0f;
+		GLfloat				_min_ry = -180.0f;
+		GLfloat				_max_rz = 180.0f;
+		GLfloat				_min_rz = -180.0f;
 };
 
 #endif
